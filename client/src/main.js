a3e3d5c9591dff034bc19032769f027c3fd169a8
Andrax <artemiy.oleynikov1982@gmail.com>
2023-03-25 01:16:57 +0300

    Bootstrap added

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import Vue from "vue";
 import App from './App.vue'
-
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import {createApp} from 'vue'
+import './app.scss'
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
+Vue.use(BootstrapVue)
+
+Vue.use(IconsPlugin)
 createApp(App).mount('#app')
