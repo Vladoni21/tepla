ee202ca2e75cc6af3f7bae67bbed5e6fc7d69c51
Лымбин Юрий <playday8787yls0@gmail.com>
2023-05-04 22:05:10 +0300

    preload fixed

--- client/src/views/Plug.vue
+++ client/src/views/Plug.vue
@@ -2,23 +2,23 @@
   <section>
     <section class="figure">
       <article class="container">
-      <slot>
-          <Error v-if="true"/>
-          <Preload v-if="false"/>
-      </slot>
-      <div class="message">
+        <slot>
+          <Error v-if="false"/>
+          <Preload v-if="true"/>
+        </slot>
+        <div class="message" v-if="false">
           <h1>404</h1>
           <p>It looks like you're lost...<br/>That's a trouble?</p>
           <button type="button">Go back</button>
         </div>
-      <article class="field">
-        <div class="bubble"></div>
-        <div class="bubble"></div>
-        <div class="bubble"></div>
-        <div class="bubble"></div>
-        <div class="bubble"></div>
+        <article class="field">
+          <div class="bubble"></div>
+          <div class="bubble"></div>
+          <div class="bubble"></div>
+          <div class="bubble"></div>
+          <div class="bubble"></div>
+        </article>
       </article>
-    </article>
     </section>
   </section>
 </template>
