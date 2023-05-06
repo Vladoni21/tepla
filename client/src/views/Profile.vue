8a1f88823793e959fe628b856486ea4847b0c342
Лымбин Юрий <playday8787yls0@gmail.com>
2023-05-06 20:39:32 +0300

    new cd fot profile

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,58 +1,12 @@
 <template>
     <section class="grid">
-        <article>
+        <article style="max-width: 420px;">
             <Image/>
         </article>
-        <article class="container">
-            <form>
-            <div class="row">
-                <h4>Account</h4>
-                <div class="input-group input-group-icon">
-                    <input type="text" placeholder="Full Name"/>
-                    <div class="input-icon"><i class="fa fa-user"></i></div>
-                </div>
-                <div class="input-group input-group-icon">
-                    <input type="email" placeholder="Email Adress"/>
-                    <div class="input-icon"><i class="fa fa-envelope"></i></div>
-                </div>
-                <div class="input-group input-group-icon">
-                    <input type="password" placeholder="Password"/>
-                    <div class="input-icon"><i class="fa fa-key"></i></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-half">
-                    <h4>Date of Birth</h4>
-                    <div class="input-group">
-                        <div class="col-third">
-                            <input type="text" placeholder="DD"/>
-                        </div>
-                        <div class="col-third">
-                            <input type="text" placeholder="MM"/>
-                        </div>
-                        <div class="col-third">
-                            <input type="text" placeholder="YYYY"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-half">
-                    <h4>Gender</h4>
-                    <div class="input-group">
-                        <input id="gender-male" type="radio" name="gender" value="male"/>
-                        <label for="gender-male">Male</label>
-                        <input id="gender-female" type="radio" name="gender" value="female"/>
-                        <label for="gender-female">Female</label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <button type="submit" style="background: rgb(59, 130, 246);" class="button transition ease-in duration-300">
-                    Подтвердить
-                </button>
-            </div>
-        </form>
+        <article style="max-width: 550px; max-height: 500px">
+            <Account/>
         </article>
-        <article class="container">
+        <article style="max-width: 370px;">
             <Badge/>
         </article>
     </section>
@@ -61,238 +15,34 @@
 <script>
 import Image from "../components/containers/Image.vue";
 import Badge from "../components/containers/Badge.vue";
+import Account from "../components/containers/Account.vue";
 
 export default {
     name: "Profile",
-    components: {Image, Badge}
+    components: {Account, Image, Badge}
 }
 </script>
 
 <style scoped>
 
-.button {
-    width: 100%;
-    margin-top: 1.25rem;
-    margin-bottom: 1.25rem;
-    padding: 1rem;
-    border-radius: 50px;
-
-    cursor: pointer;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    --tw-text-opacity: 1;
-    background: rgb(59 130 246 / var(--tw-text-opacity));
-    color: rgb(243 244 246 / var(--tw-text-opacity));
-
-    --tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-    --tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color), 0 4px 6px -4px var(--tw-shadow-color);
-    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-
-    letter-spacing: 0.025em;
-    font-weight: 600;
-}
-
 .grid {
     display: flex;
-    justify-content: space-between;
-    width: 1140px;
+    justify-content: space-around;
+    align-items: flex-start;
+    width: 1540px;
+    flex-flow: row wrap;
+
 }
 
-h4 {
-    color: rgb(59, 130, 246);
-}
-input,
-input[type="radio"] + label,
-input[type="checkbox"] + label:before,
-select option,
-select {
-    width: 100%;
-    padding: 1em;
-    line-height: 1.4;
-    background-color: #f9f9f9;
-    border: 1px solid #e5e5e5;
-    border-radius: 3px;
-    -webkit-transition: 0.35s ease-in-out;
-    -moz-transition: 0.35s ease-in-out;
-    -o-transition: 0.35s ease-in-out;
-    transition: 0.35s ease-in-out;
-    transition: all 0.35s ease-in-out;
-}
-input:focus {
-    outline: 0;
-    border-color: rgb(59, 130, 246);
-}
-input:focus + .input-icon:after + .input-icon i {
-    border-right-color: rgb(59, 130, 246);
-}
-input[type="radio"] {
-    display: none;
-}
-input[type="radio"] + label,
-select {
-    display: inline-block;
-    width: 50%;
-    text-align: center;
-    float: left;
-    border-radius: 0;
-}
-input[type="radio"] + label:first-of-type {
-    border-top-left-radius: 3px;
-    border-bottom-left-radius: 3px;
-}
-input[type="radio"] + label:last-of-type {
-    border-top-right-radius: 3px;
-    border-bottom-right-radius: 3px;
-}
-input[type="radio"] + label i {
-    padding-right: 0.4em;
-}
-input[type="radio"]:checked + label,
-input:checked + label:before,
-select:focus,
-select:active {
-    background-color: rgb(59, 130, 246);
-    color: #fff;
-    border-color: rgb(23, 93, 208);
-}
-input[type="checkbox"] {
-    display: none;
-}
-input[type="checkbox"] + label {
-    position: relative;
-    display: block;
-    padding-left: 1.6em;
-}
-input[type="checkbox"] + label:before {
-    position: absolute;
-    top: 0.2em;
-    left: 0;
-    display: block;
-    width: 1em;
-    height: 1em;
-    padding: 0;
-    content: "";
-}
-input[type="checkbox"] + label:after {
-    position: absolute;
-    top: 0.45em;
-    left: 0.2em;
-    font-size: 0.8em;
-    color: #fff;
-    opacity: 0;
-    content: "\f00c";
-}
-input:checked + label:after {
-    opacity: 1;
-}
-select {
-    height: 3.4em;
-    line-height: 2;
-}
-select:first-of-type {
-    border-top-left-radius: 3px;
-    border-bottom-left-radius: 3px;
-}
-select:last-of-type {
-    border-top-right-radius: 3px;
-    border-bottom-right-radius: 3px;
-}
-select:focus,
-select:active {
-    outline: 0;
-}
-select option {
-    background-color: #f0a500;
-    color: #fff;
-}
-.input-group {
-    margin-bottom: 1em;
-    zoom: 1;
-}
-.input-group:before,
-.input-group:after {
-    content: "";
-    display: table;
-}
-.input-group:after {
-    clear: both;
-}
-.input-group-icon {
-    position: relative;
-}
-.input-group-icon input {
-    padding-left: 4.4em;
-}
-.input-group-icon .input-icon {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 3.4em;
-    height: 3.4em;
-    line-height: 3.4em;
-    text-align: center;
-    pointer-events: none;
-}
-.input-group-icon .input-icon:after {
-    position: absolute;
-    top: 0.6em;
-    bottom: 0.6em;
-    left: 3.4em;
-    display: block;
-    border-right: 1px solid #e5e5e5;
-    content: "";
-    -webkit-transition: 0.35s ease-in-out;
-    -moz-transition: 0.35s ease-in-out;
-    -o-transition: 0.35s ease-in-out;
-    transition: 0.35s ease-in-out;
-    transition: all 0.35s ease-in-out;
-}
-.input-group-icon .input-icon i {
-    -webkit-transition: 0.35s ease-in-out;
-    -moz-transition: 0.35s ease-in-out;
-    -o-transition: 0.35s ease-in-out;
-    transition: 0.35s ease-in-out;
-    transition: all 0.35s ease-in-out;
-}
-.container {
-    max-width: 38em;
+.grid article {
+    max-width: 36em;
     padding: 1em 3em 2em 3em;
-    margin: 0em auto;
+    margin: 0 auto;
     background-color: #fff;
     border-radius: 4.2px;
-    box-shadow: 0px 3px 10px -2px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 3px 10px -2px rgba(0, 0, 0, 0.2);
 }
 
-.row {
-    zoom: 1;
-}
-.row:before,
-.row:after {
-    content: "";
-    display: table;
-}
-.row:after {
-    clear: both;
-}
-.col-half {
-    padding-right: 10px;
-    float: left;
-    width: 50%;
-}
-.col-half:last-of-type {
-    padding-right: 0;
-}
-.col-third {
-    padding-right: 10px;
-    float: left;
-    width: 33.33333333%;
-}
-.col-third:last-of-type {
-    padding-right: 0;
-}
 @media only screen and (max-width: 540px) {
     .col-half {
         width: 100%;
