7477deb7f38bddf2eda734f37245a2b6cd0a3f11
Andrax <artemiy.oleynikov1982@gmail.com>
2023-05-07 20:22:30 +0300

    footer new material icons and localization

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,57 +3,57 @@
     <div class="content">
       <div class="top">
         <div class="logo-details">
-          <i class="fab fa-slack"></i>
+          <i class="fa fa-slack"></i>
           <span class="logo_name">TeaPLA</span>
         </div>
         <div class="media-icons">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-linkedin-in"></i></a>
-          <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-vk"></i></a>
+            <a href="#"><i class="fa fa-youtube"></i></a>
         </div>
       </div>
       <br>
       <div class="link-boxes">
         <ul class="box">
-          <li class="link_name">Company</li>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Contact us</a></li>
-          <li><a href="#">About us</a></li>
-          <li><a href="#">Get started</a></li>
+          <li class="link_name">Компания</li>
+          <li><a href="#">Домой</a></li>
+          <li><a href="#">Контакты</a></li>
+          <li><a href="#">О нас</a></li>
+          <li><a href="#">Начать</a></li>
         </ul>
         <ul class="box">
-          <li class="link_name">Services</li>
+          <li class="link_name">Сервисы</li>
           <li><a href="#">App design</a></li>
           <li><a href="#">Web design</a></li>
           <li><a href="#">Logo design</a></li>
           <li><a href="#">Banner design</a></li>
         </ul>
         <ul class="box">
-          <li class="link_name">Account</li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">My account</a></li>
-          <li><a href="#">Prefrences</a></li>
-          <li><a href="#">Purchase</a></li>
+          <li class="link_name">Аккаунт</li>
+          <li><a href="#">Профиль</a></li>
+          <li><a href="#">Мой Аккаунт</a></li>
+          <li><a href="#">Предпочтения</a></li>
+          <li><a href="#">Покупки</a></li>
         </ul>
         <ul class="box">
-          <li class="link_name">Courses</li>
+          <li class="link_name">Курсы</li>
           <li><a href="#">HTML & CSS</a></li>
           <li><a href="#">JavaScript</a></li>
-          <li><a href="#">Photography</a></li>
+          <li><a href="#">Django</a></li>
           <li><a href="#">Photoshop</a></li>
         </ul>
         <ul class="box input-box">
-          <li class="link_name">Subscribe</li>
-          <li><input type="text" placeholder="Enter your email"></li>
-          <li><input type="button" value="Subscribe"></li>
+          <li class="link_name">Узнать больше</li>
+          <li><input type="text" placeholder="Введите ваш email"></li>
+          <li><input type="button" value="Подписаться"></li>
         </ul>
       </div>
     </div>
     <div class="bottom-details">
       <div class="bottom_text">
-        <span class="copyright_text">Copyright © 2021 <a href="#">CodingLab.</a>All rights reserved</span>
+        <span class="copyright_text">Copyright © 2021 <a href="#">TeaPLA</a>All rights reserved</span>
         <span class="policy_terms">
           <a href="#">Privacy policy</a>
           <a href="#">Terms & condition</a>
