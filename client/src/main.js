ae067ce3d47b073301d2336730dc83a732ffee21
genken <2024genken.gf@student.letovo.ru>
2023-05-14 20:27:58 +0300

    styled main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import App from './App.vue'
 
 import {createApp} from 'vue'
 
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
