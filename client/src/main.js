d93d7dc67ee3d2c6e8b373b72061f44ebf3399ec
genken <2024genken.gf@student.letovo.ru>
2023-05-14 19:37:12 +0300

    removed unnecessary imports

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,6 @@ import {createApp} from 'vue'
 import {fas} from "@fortawesome/free-solid-svg-icons";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -14,9 +13,6 @@ import './app.scss'
 
 
 Vue.config.productionTip = false
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 library.add(fas)
 createApp(App).component('fa', FontAwesomeIcon).mount('#app')
