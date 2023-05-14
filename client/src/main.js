3736578115336dc155114d765b638f758fcbc743
genken <2024genken.gf@student.letovo.ru>
2023-05-14 23:12:35 +0300

    added App.vue to use router

--- client/src/main.js
+++ client/src/main.js
@@ -8,14 +8,15 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import './app.scss'
 
+import router from './router'
 
 Vue.config.productionTip = false
 
 library.add(fas)
 
 const app = createApp(App)
+app.use(router)
 app.component('fa', FontAwesomeIcon)
 app.mount('#app')
