e7725cddec1992b1f2f3db22adcc619732be6788
genken <2024genken.gf@student.letovo.ru>
2023-05-21 15:02:13 +0300

    added test User register endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const db = require('../db');
 
 // const controller = require('../controllers/auth.controller');
 
 const router = new express.Router();
 
-// router.post('/registration', controller().registration);
+router.post('/register', (req, res) => {
+    db.models.User.create(
+        req.body,
+        {
+            fields: ['username', 'email', 'password']
+        }
+    )
+        .then(
+            user => {
+                res.status(201);
+            }
+        )
+        .catch(
+            error => {
+                res.status(500);
+            }
+        );
+});
+
+router.post('/login', async (req, res) => {
+
+});
 
 module.exports = router;
